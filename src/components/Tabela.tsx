6d7e179b6d6d959e830246375dd6a18d5e0ca419
Ana Beatriz <anabiawalker@gmail.com>
2021-12-23 12:16:27 -0300

    Aplciando estilo no Componente Tabela

--- src/components/Tabela.tsx
+++ src/components/Tabela.tsx
@@ -8,9 +8,9 @@ export default function Tabela(props: TabelaProps){
 
     function renderCabecalho(){
         <tr>
-            <th>Código</th>
-            <th>Nome</th>
-            <th>Idade</th>
+            <th className='text-left p-4'>Código</th>
+            <th className='text-left p-4'>Nome</th>
+            <th className='text-left p-4'>Idade</th>
         </tr>
     }
 
@@ -18,10 +18,11 @@ export default function Tabela(props: TabelaProps){
         /*se cliente estiver preenchido*/
         return props.clientes?.map((cliente, i)=>{
             return (
-                <tr key={cliente.id}>
-                    <td>{cliente.id}</td>
-                    <td>{cliente.nome}</td>
-                    <td>{cliente.idade}</td>
+                <tr key={cliente.id} 
+                    className={`${i % 2 === 0 ?'bg-purple-200': 'bg-purple-100'}`}>
+                    <td className='text-left p-4'>{cliente.id}</td>
+                    <td className='text-left p-4'>{cliente.nome}</td>
+                    <td className='text-left p-4'>{cliente.idade}</td>
                 </tr>
             )
         })
@@ -29,8 +30,11 @@ export default function Tabela(props: TabelaProps){
     }
 
     return (
-        <table>
-            <thead>
+        <table className='w-full rounded-xl overflow-hidden'>
+            <thead className={`
+                text-gray-100
+                bg-gradient-to-r from-purple-500 to-purple-700
+            `}>
                 {renderCabecalho}
             </thead>
             <tbody>
