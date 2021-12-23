363f8a2ed9167e9b7e6713ab1364dd96e28d2c01
Ana Beatriz <anabiawalker@gmail.com>
2021-12-23 11:44:02 -0300

    Adc Classe Cliente

---
+++ src/core (dominios)/Cliente.ts
@@ -0,0 +1,27 @@
+export default class Cliente{
+    #id: string
+    #nome: string
+    #idade: number
+    //# = private
+
+    constructor(nome: string, idade: number, id: string = null){
+        this.#nome = nome
+        this.#idade - idade
+        this.#id = id
+    }
+
+    //Cliente vazio
+    static vazio(){
+        return new Cliente('', 0)
+    }
+
+    get id(){
+        return this.id
+    }
+    get nome(){
+        return this.nome
+    }
+    get idade(){
+        return this.idade
+    }
+}
