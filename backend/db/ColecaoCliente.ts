04d7d0cdfd6f79b3f119ee055080ec398bdd2bac
Ana Beatriz <anabiawalker@gmail.com>
2021-12-27 14:52:52 -0300

    Adc métodos salvar, exlcuir e obterTodos

--- backend/db/ColecaoCliente.ts
+++ backend/db/ColecaoCliente.ts
@@ -18,13 +18,35 @@ export default class ColecaoCliente implements ClienteRepositorio{
     }
 
     async salvar(cliente: Cliente): Promise<Cliente>{
-        return null
+        //Se há um id, vai alterar
+        if(cliente?.id){
+            await this.colecao().doc(cliente.id).set(cliente)
+            return cliente //cliente já convertido para objeto
+
+        //Senão, irá adicionar os dados
+        }else{
+            //Pega a referencia do documento
+            const docRef = await this.colecao().add(cliente)
+
+            //Pega o snapshot do documento
+            const doc = await docRef.get()
+
+            //Retorna os dados do documento
+            return doc.data()
+        }
     }
+
     async excluir(cliente: Cliente): Promise<void>{
-        return null
+        //Acessando um documento de um cliente pelo id
+        return this.colecao().doc(cliente.id).delete()
     }
+
     async obterTodos(): Promise<Cliente[]>{
-        return null
+        //Pega um query snapshot
+        const query = await this.colecao().get()
+
+        //A partir da query pega vários documentos, mapeia cada e pega o objeto data que são os próprios clientes
+        return query.docs.map(doc => doc.data()) ?? []
     }
 
     private colecao(){
